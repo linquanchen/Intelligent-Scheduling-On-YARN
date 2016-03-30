1d3e2a8259143897101c1810d7c65a15898b3d64
Jeffery <lqchen613@gmail.com>
2016-03-30 15:04:21 -0400

    modify the enviro file

--- envir.sh
+++ envir.sh
@@ -3,7 +3,7 @@
 #################################################################
 # Before run the shell, add a file called masterip contains the #
 # master VM ip; modify the file called slavevmips contains all  #
-# ips of slaves.                                                 #
+# ips of slaves.                                                #
 #################################################################
 
 
@@ -13,9 +13,9 @@
 
 # set up environment variables in ~/.bashrc
 ./mpssh -u root -s -f vmips "echo \"export HADOOP_HOME=/opt/projects/advcc/hadoop/hadoop-2.2.0\" >> ~/.bashrc"
-./mpssh -u root -s -f vmips "echo \"export HADOOP_CONF_DIR=${HADOOP_HOME}/etc/hadoop\" >> ~/.bashrc"
-./mpssh -u root -s -f vmips "echo \"export HADOOP_PREFIX=$HADOOP_HOME\" >> ~/.bashrc"
-./mpssh -u root -s -f vmips "echo \"export HADOOP_YARN_HOME=$HADOOP_HOME\" >> ~/.bashrc"
+./mpssh -u root -s -f vmips "echo \"export HADOOP_CONF_DIR=\${HADOOP_HOME}/etc/hadoop\" >> ~/.bashrc"
+./mpssh -u root -s -f vmips "echo \"export HADOOP_PREFIX=\$HADOOP_HOME\" >> ~/.bashrc"
+./mpssh -u root -s -f vmips "echo \"export HADOOP_YARN_HOME=\$HADOOP_HOME\" >> ~/.bashrc"
 ./mpssh -u root -s -f vmips "echo \"export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64\" >> ~/.bashrc"
 ./mpssh -u root -s -f vmips "source ~/.bashrc"
 
@@ -23,15 +23,15 @@
 ./mpssh -u root -s -f vmips "/opt/projects/advcc/hadoop/scripts/setup-scratch-user.sh" 
 
 # On master
-./mpssh -u root -s -f masterip "$HADOOP_PREFIX/bin/hdfs namenode -format Phase1Team10"
-./mpssh -u root -s -f masterip "$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode"
-./mpssh -u root -s -f masterip "$HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager"
-./mpssh -u root -s -f masterip "$HADOOP_YARN_HOME/sbin/yarn-daemon.sh start proxyserver --config $HADOOP_CONF_DIR"
-./mpssh -u root -s -f masterip "$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver --config $HADOOP_CONF_DIR"
+./mpssh -u root -s -f masterip "\$HADOOP_PREFIX/bin/hdfs namenode -format Phase1Team10"
+./mpssh -u root -s -f masterip "\$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config \$HADOOP_CONF_DIR --script hdfs start namenode"
+./mpssh -u root -s -f masterip "\$HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config \$HADOOP_CONF_DIR start resourcemanager"
+./mpssh -u root -s -f masterip "\$HADOOP_YARN_HOME/sbin/yarn-daemon.sh start proxyserver --config \$HADOOP_CONF_DIR"
+./mpssh -u root -s -f masterip "\$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver --config \$HADOOP_CONF_DIR"
 
 # On all the slaves
-./mpssh -u root -s -f slavevmips "$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP-CONF_DIR --script hdfs start datanode"
-./mpssh -u root -s -f slavevmips "$HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start nodemanager"
+./mpssh -u root -s -f slavevmips "\$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config \$HADOOP_CONF_DIR --script hdfs start datanode"
+./mpssh -u root -s -f slavevmips "\$HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config \$HADOOP_CONF_DIR start nodemanager"
 
 # On the master, start the node manager
-./mpssh -u root -s -f slavevmips "$HADOOP_HOME/sbin/yarn-daemon.sh  --config $HADOOP_CONF_DIR/amnode start nodemanager"
+./mpssh -u root -s -f slavevmips "\$HADOOP_HOME/sbin/yarn-daemon.sh  --config \$HADOOP_CONF_DIR/amnode start nodemanager"
